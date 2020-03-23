2ef561b79486c672a61b05d672841422693a5915
Aaron Ky-Riesenbach <aaronk2002@pobox.com>
2020-03-23 00:26:48 -0400

    Create generatePossibleRows for Problem 215

---
+++ Problem215/Problem215.py
@@ -0,0 +1,39 @@
+# Consider the problem of building a wall out of 2×1 and 3×1 bricks (horizontal × vertical dimensions) such that, for extra strength, the gaps between horizontally-adjacent
+# bricks never line up in consecutive layers, i.e. never form a "running crack".
+# There are eight ways of forming a crack-free 9×3 wall, written W(9,3) = 8.
+# Calculate W(32,10).
+
+def generatePossibleRows(row, width): #generates all possible rows using bricks of length 2 and 3 given initial row (for recursion) and width of row
+    rows = open("%srows.txt" %width, "a")
+    length = 0
+    for i in row:
+        length += i
+    if length == width - 4:
+        row.append(2)
+        row.append(2)
+        strRow = ''
+        for brick in row:
+            strRow += str(brick)
+        rows.write(strRow + '\n')
+    elif length == width - 3:
+        row.append(3)
+        strRow = ''
+        for brick in row:
+            strRow += str(brick)
+        rows.write(strRow + '\n')
+    elif length == width - 2:
+        row.append(2)
+        strRow = ''
+        for brick in row:
+            strRow += str(brick)
+        rows.write(strRow + '\n')
+    elif length < width - 4:
+        row1 = row.copy()
+        row2 = row.copy()
+        row1.append(2)
+        row2.append(3)
+        generatePossibleRows(row1, width)
+        generatePossibleRows(row2, width)
+    rows.close()
+
+generatePossibleRows([], 32)
