3983c8b4b2020e30c3bc032f309523b99c8bf4c6
Aaron Ky-Riesenbach <aaronk2002@pobox.com>
2020-03-22 22:38:39 -0400

    Begin problem 215

---
+++ Problem 215.py
@@ -0,0 +1,5 @@
+# Consider the problem of building a wall out of 2×1 and 3×1 bricks (horizontal × vertical dimensions) such that, for extra strength, the gaps between horizontally-adjacent
+# bricks never line up in consecutive layers, i.e. never form a "running crack".
+# There are eight ways of forming a crack-free 9×3 wall, written W(9,3) = 8.
+# Calculate W(32,10).
+
